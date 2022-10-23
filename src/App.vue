fdb6f24e32409969cb58aa66ab1eb078376b93a7
jonathan <Jmerol27@gmail.com>
2022-10-23 12:51:20 -0500

    se realizan cambio y me quede en el state

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref , reactive } from 'vue'
+  import Header from './components/Header.vue'
+  
+  const cantidad = ref(0);
+
+  const state = reactive({
+    cantidad: 0
+  })
+
+  function handreChange(e) {
+    cantidad.value = +e.target.value;
+  }
+
+  console.log(cantidad.value);
+  console.log(state.cantidad);
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  
+    <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header />
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+      <div class="my-5">
+        <input 
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        @input="handreChange"
+        />
+      </div>
+
+      {{cantidad}}
+
+    </div>
+  
+</template>
