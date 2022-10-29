7527df62e38324b312062a14b7270cea7f083474
jonathan <Jmerol27@gmail.com>
2022-10-28 20:01:53 -0500

    actualizacion

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <script setup>
-  import { ref , reactive } from 'vue'
+  import { ref, computed } from 'vue'
   import Header from './components/Header.vue'
+  import Button from './components/Button.vue'
   
-  const cantidad = ref(0);
+  const cantidad = ref(10000);
 
-  const state = reactive({
-    cantidad: 0
-  })
+  const MIN = 0 ;
+  const MAX = 20000 ;
+  const STEP = 100 ;
 
-  function handreChange(e) {
-    cantidad.value = +e.target.value;
+  const formateardinero = computed(() => {
+    const formatter = new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD'
+    });
+    
+    return formatter.format(cantidad.value);
+
+  });
+
+  const handleChangeDecremento = () => {
+
+    const valor =  cantidad.value - STEP
+
+    if (valor < MIN) {
+      alert('Cantidad no valida');
+      return;
+    }
+
+    cantidad.value = valor;
+    
   }
+  const handleChangeIncremento = () => {
 
-  console.log(cantidad.value);
-  console.log(state.cantidad);
+    const valor =  cantidad.value + STEP
+
+    if (valor > MAX) {
+      alert('Cantidad no valida');
+      return;
+    }
+
+    cantidad.value = valor;
+    
+  }
 
 </script>
 
@@ -22,15 +51,45 @@
     <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
 
+      <div class="flex justify-between mt-10">
+      
+        <Button
+        
+          :operador="'-'"
+          @fn="handleChangeDecremento"
+        />
+         
+        <Button 
+        
+          :operador="'+'"
+          @fn="handleChangeIncremento"
+        />
+
+      </div>
+    
+
       <div class="my-5">
         <input 
         type="range"
         class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
-        @input="handreChange"
+        :min="MIN"
+        :max="MAX"
+        :step="STEP"
+        v-model.number="cantidad"
         />
+
+        <p class="text-center my-10 text-5xl font font-extrabold text-indigo-600">
+          {{formateardinero}}
+        </p>
+
+        <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+          Elige un <span class="text-indigo-600">Plazo</span> a pagar
+        </h2>
+
       </div>
 
-      {{cantidad}}
+      
+      
 
     </div>
   
